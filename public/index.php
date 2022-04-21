34d8eff18c5d9bbe52f1eed64cc272201a400a78
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-21 17:19:16 +0300

    Korjattu include-polut

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
-include_once(TEMPLATES_DIR . "head.php")
+include_once(TEMPLATES_DIR . "head.php");
 ?>
 
 <main>CONTENT</main>
 
 <?php
-include_once(TEMPLATES_DIR . "foot.php")
+include_once(TEMPLATES_DIR . "foot.php");
 ?>
