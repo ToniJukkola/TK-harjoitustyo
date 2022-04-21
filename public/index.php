2e3f17f28aa6330621504fc91d16e151bb76a591
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-21 19:31:20 +0300

    Lisätty vähän turhia tyylejä

--- public/index.php
+++ public/index.php
@@ -2,7 +2,17 @@
 include_once(TEMPLATES_DIR . "head.php");
 ?>
 
-<main>CONTENT</main>
+<main>
+
+<h2>h2</h2>
+<h3>h3</h3>
+  
+<h3>Alert boxeja!!</h3>
+<div class="alert">.alert</div>
+<div class="alert alert-success">.alert .alert-success</div>
+<div class="alert alert-fail">.alert .alert-fail</div>
+
+</main>
 
 <?php
 include_once(TEMPLATES_DIR . "foot.php");
