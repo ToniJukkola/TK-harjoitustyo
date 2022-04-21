d743393a669dba3f5a3c5a6d8f301609151bcad0
TIINA PELIMANNI <tiina.pelimanni@gmail.com>
2022-04-21 13:56:53 +0300

    Publiciin luotu perusnavigaatio

---
+++ public/tehtavat.php
@@ -0,0 +1,9 @@
+<?php
+include_once("../src/templates/head.php")
+?>
+
+<main>Tehtävät</main>
+
+<?php
+include_once("../src/templates/foot.php")
+?>
--- src/templates/head.php
+++ src/templates/head.php
@@ -5,18 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Group Work Manager</title>
+    <title>Ryhmätyömanageri</title>
     <link rel="stylesheet" href="../public/css/styles.css">
 </head>
 
 <body>
     <header>
-        <h1>Group Work Manager</h1>
+        <h1>Ryhmätyömanageri</h1>
         <nav>
             <ul>
-                <li><a href="">Linkki</a></li>
-                <li><a href="">Linkki</a></li>
-                <li><a href="">Linkki</a></li>
+                <li><a href="tyypit.php">Tyypit</a></li>
+                <li><a href="projektit.php">Projektit</a></li>
+                <li><a href="tehtavat.php">Tehtävät</a></li>
+                <li><a href="kirjaudu.php">Kirjaudu / rekisteröidy</a></li>
             </ul>
         </nav>
     </header>
