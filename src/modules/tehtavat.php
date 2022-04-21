4a05a2587593f0d1cba013b75a89604888493832
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-21 17:23:08 +0300

    Poistettu ylimääräinen sarake sql-lauseesta

--- src/modules/tehtavat.php
+++ src/modules/tehtavat.php
@@ -6,7 +6,7 @@ function getTasks()
 
     try {
         $pdo = connectToDatabase();
-        $sql = "SELECT task.id AS task_id, task_name, CONCAT(SUBSTRING(due_date, 9, 2), '.', SUBSTRING(due_date, 6, 2), '.', YEAR(due_date)) as due_date, date_created, date_finished, priority_level, project_name, is_completed FROM task
+        $sql = "SELECT task.id AS task_id, task_name, CONCAT(SUBSTRING(due_date, 9, 2), '.', SUBSTRING(due_date, 6, 2), '.', YEAR(due_date)) as due_date, date_created, date_finished, priority_level, project_name FROM task
         LEFT JOIN project ON project.id = task.project_id";
         $tasks = $pdo->query($sql);
         return $tasks->fetchAll();
