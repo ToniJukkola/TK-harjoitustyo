2f7842473a74a0a39a39d2c9202c964d92526d15
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-04-22 16:08:12 +0300

    Salasanan tarkistus lisätty rekisteröitymisessä.

--- public/kirjaudu.php
+++ public/kirjaudu.php
@@ -4,6 +4,7 @@ include_once MODULES_DIR.'account-control.php';
 
 $username = filter_input(INPUT_POST, "username");
 $password = filter_input(INPUT_POST, "password");
+$passwordCheck = filter_input(INPUT_POST, "passwordCheck");
 $formType = filter_input(INPUT_POST, "formType");
 $firstName = filter_input(INPUT_POST, "firstName");
 $lastName = filter_input(INPUT_POST, "lastName");
@@ -28,7 +29,7 @@ if(!isset($_SESSION["username"]) && isset($username) && isset($formType)) {
         } catch (Exception $e) {
             echo '<div class="alert alert-fail">'.$e->getMessage().'</div>';
         }
-    } else if ($formType == 1) {
+    } else if ($formType == 1 && $passwordCheck === $password) {
         try {
             register($username, $password, $firstName, $lastName);
             login($username, $password);
@@ -37,43 +38,54 @@ if(!isset($_SESSION["username"]) && isset($username) && isset($formType)) {
         } catch (Exception $e) {
             echo '<div class="alert alert-fail">'.$e->getMessage().'</div>';
         }
+    } else if($passwordCheck !== $password) {
+        echo '<div class="alert alert-fail">Passwords do not match</div>';
     }
 }
 
 if(!isset($_SESSION["username"])) {
 ?>
 
-<main>
+<main >
     <form action="kirjaudu.php" method="post">
         <div>
-            <input type="radio" name="formType" id="loginRadio" value="0" checked>
+            <input type="radio" name="formType" id="loginRadio" value="0" onclick="handler('0')" checked>
             <label for="loginRadio">Kirjaudu</label>
-            <input type="radio" name="formType" id="registerRadio" value="1">
+            <input type="radio" name="formType" id="registerRadio" value="1" onclick="handler('1')">
             <label for="registerRadio">Rekisteröidy</label>
             
         </div>
-        <label for="username">Sähköposti: </label>
-        <input id="username" name="username" type="text">
-        <label for="password">Salasana: </label>
-        <input id="password" name="password" type="password">
-        <button type="submit">Kirjau-u</button>
-        <div>Ei tunnuksii?</div>
         <div>
-            <label for="regPasswordAgain">Salasana uudestaan :DDD </label>
-            <input id="regPasswordAgain" type="password">
+            <label for="username">Sähköposti: </label>
+            <input id="username" name="username" type="text">
         </div>
         <div>
-            <label for="firstName">Etunimi: </label>
-            <input id="firstName" name="firstName" type="text">
+            <label for="password">Salasana: </label>
+            <input id="password" name="password" type="password">
         </div>
         <div>
-            <label for="lastName">Sukuloimisnimi: </label>
-            <input id="lastName" name="lastName" type="text">
+            <button type="submit" class="register-toggle">Kirjaudu</button>
         </div>
-        <div>
-            <button type="submit">Reggaroi</button>
+        
+        <div class="register-toggle hidden">
+            <div>
+                <label for="passwordCheck">Salasana uudelleen: </label>
+                <input id="passwordCheck" name="passwordCheck" type="password">
+            </div>
+            <div>
+                <label for="firstName">Etunimi: </label>
+                <input id="firstName" name="firstName" type="text">
+            </div>
+            <div>
+                <label for="lastName">Sukunimi: </label>
+                <input id="lastName" name="lastName" type="text">
+            </div>
+            <div>
+                <button type="submit">Rekisteröidy</button>
+            </div>
         </div>
     </form>
+    <script src="./js/account.js"></script>
 </main>
 
 <?php }
