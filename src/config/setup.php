6f7e73c62e8fcc4565809f5cde3adf71d303d003
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-22 14:08:29 +0300

    Faviconit >:)

--- src/config/setup.php
+++ src/config/setup.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SETUP -> Ryhmätyömanageri</title>
+  <link rel="icon" href="../../public/images/favicon-setup.png" type="image/png">
+</head>
+<body>
 <?php
 require_once("dbconn.php");
 require_once("sql-variables.php");
@@ -32,3 +42,7 @@ try {
   throw $pdoex;
   echo $pdoex;
 }
+?>
+
+</body>
+</html>
--- src/templates/head.php
+++ src/templates/head.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ryhmätyömanageri</title>
     <link rel="stylesheet" href="../public/css/styles.css">
+    <link rel="icon" href="../public/images/favicon-public.png" type="image/png">
 </head>
 
 <body>
