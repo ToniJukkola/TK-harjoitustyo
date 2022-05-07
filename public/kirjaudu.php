27710324c2fd4fe2f4d4feff47e20097b3f0adf8
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-05-07 21:18:09 +0300

    Kirjautumisen tarkistuksia. Kirjautuminen onnistuu myös s-postilla. Rekisteröitymisessä voi asettaa tunnuksen.

--- public/kirjaudu.php
+++ public/kirjaudu.php
@@ -8,8 +8,92 @@ $passwordCheck = filter_input(INPUT_POST, "passwordCheck");
 $formType = filter_input(INPUT_POST, "formType");
 $firstName = filter_input(INPUT_POST, "firstName");
 $lastName = filter_input(INPUT_POST, "lastName");
+$email = filter_input(INPUT_POST, "email");
 
-if(isset($_SESSION["username"])) {
+if(!isset($_SESSION["username"])) {
+
+    ?>
+    <main>
+    <?php
+
+    if(isset($username) && isset($formType)) {
+
+        if(filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            $row = findUserByEmail($username);
+            $username = $row["username"];
+        }
+
+        if ($formType == 0) {
+            try {
+                login($username, $password);
+                header("Location: index.php");
+                exit;
+            } catch (Exception $e) {
+                echo '<div class="alert alert-fail">'.$e->getMessage().'</div>';
+            }
+        } else if ($formType == 1 && $passwordCheck === $password) {
+            try {
+                register($username, $password, $firstName, $lastName, $email);
+                login($username, $password);
+                header("Location: index.php");
+                exit;
+            } catch (Exception $e) {
+                echo '<div class="alert alert-fail">'.$e->getMessage().'</div>';
+            }
+        } else if($passwordCheck !== $password) {
+            echo '<div class="alert alert-fail">Salasanat eivät täsmää</div>';
+        }
+    }
+
+    ?>
+
+        <form action="kirjaudu.php" method="post">
+            <div>
+                <input type="radio" name="formType" id="loginRadio" value="0" onclick="handler('0')" checked>
+                <label for="loginRadio">Kirjaudu</label>
+                <input type="radio" name="formType" id="registerRadio" value="1" onclick="handler('1')">
+                <label for="registerRadio">Rekisteröidy</label>
+                
+            </div>
+            <div>
+                <label for="username">Käyttäjätunnus: </label>
+                <input id="username" name="username" type="text">
+            </div>
+            <div>
+                <label for="password">Salasana: </label>
+                <input id="password" name="password" type="password">
+            </div>
+            <div>
+                <button type="submit" class="register-toggle">Kirjaudu</button>
+            </div>
+            
+            <div class="register-toggle hidden">
+                <div>
+                    <label for="passwordCheck">Salasana uudelleen: </label>
+                    <input id="passwordCheck" name="passwordCheck" type="password">
+                </div>
+                <div>
+                    <label for="email">Sähköposti: </label>
+                    <input id="email" name="email" type="email">
+                </div>
+                <div>
+                    <label for="firstName">Etunimi: </label>
+                    <input id="firstName" name="firstName" type="text">
+                </div>
+                <div>
+                    <label for="lastName">Sukunimi: </label>
+                    <input id="lastName" name="lastName" type="text">
+                </div>
+                <div>
+                    <button type="submit">Rekisteröidy</button>
+                </div>
+            </div>
+        </form>
+        <script src="./js/account.js"></script>
+    </main>
+
+    <?php 
+} else {
     try {
         logout();
         header("Location: kirjaudu.php");
@@ -19,75 +103,5 @@ if(isset($_SESSION["username"])) {
     } 
 }
 
-if(!isset($_SESSION["username"]) && isset($username) && isset($formType)) {
-
-    if ($formType == 0) {
-        try {
-            login($username, $password);
-            header("Location: index.php");
-            exit;
-        } catch (Exception $e) {
-            echo '<div class="alert alert-fail">'.$e->getMessage().'</div>';
-        }
-    } else if ($formType == 1 && $passwordCheck === $password) {
-        try {
-            register($username, $password, $firstName, $lastName);
-            login($username, $password);
-            header("Location: index.php");
-            exit;
-        } catch (Exception $e) {
-            echo '<div class="alert alert-fail">'.$e->getMessage().'</div>';
-        }
-    } else if($passwordCheck !== $password) {
-        echo '<div class="alert alert-fail">Salasanat eivät täsmää</div>';
-    }
-}
-
-if(!isset($_SESSION["username"])) {
-?>
-
-<main >
-    <form action="kirjaudu.php" method="post">
-        <div>
-            <input type="radio" name="formType" id="loginRadio" value="0" onclick="handler('0')" checked>
-            <label for="loginRadio">Kirjaudu</label>
-            <input type="radio" name="formType" id="registerRadio" value="1" onclick="handler('1')">
-            <label for="registerRadio">Rekisteröidy</label>
-            
-        </div>
-        <div>
-            <label for="username">Sähköposti: </label>
-            <input id="username" name="username" type="text">
-        </div>
-        <div>
-            <label for="password">Salasana: </label>
-            <input id="password" name="password" type="password">
-        </div>
-        <div>
-            <button type="submit" class="register-toggle">Kirjaudu</button>
-        </div>
-        
-        <div class="register-toggle hidden">
-            <div>
-                <label for="passwordCheck">Salasana uudelleen: </label>
-                <input id="passwordCheck" name="passwordCheck" type="password">
-            </div>
-            <div>
-                <label for="firstName">Etunimi: </label>
-                <input id="firstName" name="firstName" type="text">
-            </div>
-            <div>
-                <label for="lastName">Sukunimi: </label>
-                <input id="lastName" name="lastName" type="text">
-            </div>
-            <div>
-                <button type="submit">Rekisteröidy</button>
-            </div>
-        </div>
-    </form>
-    <script src="./js/account.js"></script>
-</main>
-
-<?php }
 include_once TEMPLATES_DIR.'foot.php';
 ?>
--- src/modules/account-control.php
+++ src/modules/account-control.php
@@ -1,7 +1,7 @@
 <?php
+    require_once CONFIG_DIR.'dbconn.php';
 
 function login($username, $password) {
-    require_once CONFIG_DIR.'dbconn.php';
     $pdo = connectToDatabase();
 
     if( !isset($username) || !isset($password) ){
@@ -13,9 +13,9 @@ function login($username, $password) {
     }
 
     try{
-        $sql = "SELECT * FROM person WHERE email=:username";
+        $sql = "SELECT * FROM person WHERE username = ?";
         $statement = $pdo->prepare($sql);
-        $statement->bindParam(":username", $username);
+        $statement->bindParam(1, $username);
         $statement->execute();
 
         if($statement->rowCount() <=0){
@@ -46,9 +46,7 @@ function logout(){
     }
 }
 
-function register($username, $password, $firstName, $lastName) {
-    require_once CONFIG_DIR.'dbconn.php';
-    
+function register($username, $password, $firstName, $lastName, $email) {
     if( !isset($firstName) || !isset($lastName) || !isset($username) || !isset($password) ){
         throw new Exception('Tapahtui virhe. Kaikkia tietoja ei saatu. Ole yhteydessä "koodariin".');
     }
@@ -59,19 +57,33 @@ function register($username, $password, $firstName, $lastName) {
     
     try{
         $pdo = connectToDatabase();
-        $sql = "INSERT INTO person (firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
+        $sql = "INSERT INTO person (firstname, lastname, email, password, username) VALUES (?, ?, ?, ?, ?)";
         $statement = $pdo->prepare($sql);
         $statement->bindParam(1, $firstName);
         $statement->bindParam(2, $lastName);
-        $statement->bindParam(3, $username);
+        $statement->bindParam(3, $email);
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
         $statement->bindParam(4, $hashed_password);
+        $statement->bindParam(5, $username);
         
         $statement->execute();
-    
-        echo "Oho tähä toimi. Morjensta $firstName $lastName"; 
     }catch(PDOException $e){
         throw $e;
     }
 }
 
+function findUserByEmail($email) {
+    try{
+        $pdo = connectToDatabase();
+        $sql = "SELECT * FROM person WHERE email = ?";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(1, $email);
+        $statement->execute();
+
+        $row = $statement->fetch();
+
+        return $row;
+    }catch(PDOException $e){
+        throw $e;
+    }
+}
